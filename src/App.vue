719d63aa0a3a171104d82f69c7033bd297dcac3e
mulealem <110355554+mulealem@users.noreply.github.com>
2023-10-13 09:04:56 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<script>
+import { defineComponent } from 'vue'
+import HelloWorld from './components/HelloWorld.vue'
+
+export default defineComponent({
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+})
+</script>
+
+<template>
+  <div id="app">
+    <img src="@/assets/logo.svg" alt="Vite logo" />
+    <HelloWorld msg="Hello Vue 2 + Vite" />
+  </div>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
