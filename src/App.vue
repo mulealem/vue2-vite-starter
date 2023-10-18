3af2bc44fc0648f9e186a18246ad11c5a1650d2a
endashaw demsis <endashawdemsis@gmail.com>
2023-10-18 15:43:06 +0300

    at some point

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,5 @@
-<script>
-import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
-export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld,
-  },
-})
-</script>
-
 <template>
-  <div id="app">
-    <img src="@/assets/logo.svg" alt="Vite logo" />
-    <HelloWorld msg="Hello Vue 2 + Vite" />
+  <div class="w-full h-screen bg-red-500">
+    <router-view />
   </div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,26 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
-new Vue(App).$mount('#app')
+import router from './router'
+
+Vue.use(VueRouter)
+
+import './assets/style.css'
+
+import { LMap, LTileLayer, LMarker } from 'vue2-leaflet'
+import 'leaflet/dist/leaflet.css'
+
+import LDrawToolbar from 'vue2-leaflet-draw-toolbar'
+// ...
+Vue.component('l-draw-toolbar', LDrawToolbar)
+// ...
+
+Vue.component('l-map', LMap)
+Vue.component('l-tile-layer', LTileLayer)
+Vue.component('l-marker', LMarker)
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount('#app')
